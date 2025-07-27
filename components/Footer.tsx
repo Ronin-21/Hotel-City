99e251b33fc8aa7f0a894a01d84031e25b13f1e5
Abel Acuña <ale_lasarte@hotmail.com>
2025-07-26 21:33:48 -0300

    Change footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,8 +6,8 @@ const Footer = () => {
   return (
     <div className="flex flex-col items-center justify-center w-full bg-primary">
       <div className="container flex items-start justify-around h-full pt-16 text-lg text-white">
-        <div className="flex flex-col items-start justify-center">
-          <Link href={"/"} className="mb-4">
+        <div className="flex flex-col items-center justify-center">
+          <Link href={"/"} className="">
             <Image
               alt="logo"
               src={"/LogoCityNegativo.png"}
@@ -15,14 +15,24 @@ const Footer = () => {
               width={80}
             />
           </Link>
-          <p>Av. Belgrano 1500</p>
-          <p>Las Termas de Rio Hondo - Sgo. del Estero</p>
           <Link
             href={"/"}
-            className="px-6 py-2 mt-4 font-semibold text-white transition-colors bg-black rounded-md hover:bg-white hover:text-dark text-base"
+            className="px-6 py-2 my-4 font-semibold text-white transition-colors bg-black rounded-md hover:bg-white hover:text-dark text-base"
           >
             Reservar ahora
           </Link>
+          {/* <p className="mt-4 text-xl font-semibold">Redes</p> */}
+          <div className="flex items-center justify-between gap-4 text-2xl">
+            <Link href={"/"}>
+              <FaFacebookF />
+            </Link>
+            <Link href={"/"}>
+              <FaInstagram />
+            </Link>
+            <Link href={"/"}>
+              <FaTiktok />
+            </Link>
+          </div>
         </div>
         <div className="flex flex-col items-start justify-center gap-2">
           <p className="mb-4 text-2xl font-semibold">Links</p>
@@ -38,24 +48,14 @@ const Footer = () => {
         </div>
         <div className="flex flex-col items-start justify-center gap-2">
           <p className="mb-4 text-2xl font-semibold">Contáctos</p>
+          <p>Av. Belgrano 1500</p>
+          <p>Las Termas de Rio Hondo - Sgo. del Estero</p>
           <p>+54 9 3858 480489</p>
           <p>hotelcity@info.com.ar</p>
-          <p className="mt-4 text-xl font-semibold">Redes</p>
-          <div className="flex items-center justify-between gap-4 text-2xl">
-            <Link href={"/"}>
-              <FaFacebookF />
-            </Link>
-            <Link href={"/"}>
-              <FaInstagram />
-            </Link>
-            <Link href={"/"}>
-              <FaTiktok />
-            </Link>
-          </div>
         </div>
       </div>
       {/* Derechos y Autoria */}
-      <div className="py-3 text-center border-t border-dark/50">
+      <div className="pb-10 pt-8 mt-2 text-center  border-dark/30 border-t-2">
         <p className="text-white">
           © 2025 Hotel Ambassador · Todos los derechos reservados · Sitio
           desarrollado por Ronin WebDesign
