ec690e8338624df3469066ebeea73af16948d58c
Abel Acuña <ale_lasarte@hotmail.com>
2025-08-07 21:22:35 -0300

    Change rooms page

--- app/habitaciones/components/RoomGallery.tsx
+++ app/habitaciones/components/RoomGallery.tsx
@@ -5,26 +5,29 @@ const RoomGallery = () => {
   return (
     <div className="container flex flex-col items-center justify-center min-h-screen gap-20 mx-auto my-20">
       {/* Galeria de Habitaciones */}
-      <div className="flex items-start w-full gap-20 justify-evenly">
-        <div className="relative w-[700px] overflow-hidden rounded-lg h-[450px]">
+      <div className="flex items-center w-full justify-center gap-10 relative">
+        <div className="relative w-[700px] overflow-hidden rounded-lg h-[450px] shadow-lg shadow-black/50">
           <Image src="/room-1.jpg" fill alt="img" className="object-cover" />
         </div>
-        <div className="flex flex-col items-start max-w-xl gap-8">
-          <h2 className="text-5xl font-bold text-primary font-title text-shadow-sm text-shadow-dark/30">
-            Habitacion Deluxe
+        <div className="flex flex-col items-center justify-center text-center gap-8 max-w-xl text-dark">
+          <h2 className="text-5xl font-bold font-title text-shadow-sm text-shadow-dark/50 text-white">
+            Habitación Superior
           </h2>
           <p className="text-lg">
-            Disfruta de una estancia lujosa en nuestra habitación Deluxe,
-            equipada con todas las comodidades modernas y un diseño elegante.
-            Ideal para relajarse después de un día explorando la ciudad.
+            La Habitación Superior combina diseño moderno con detalles que
+            marcan la diferencia. Más amplia, con mejores vistas y servicios
+            exclusivos, es perfecta para una experiencia de descanso superior.
+            Ideal para escapadas románticas o estadías prolongadas con estilo y
+            confort.
           </p>
-          <div className="flex items-center justify-evenly w-full mt-5 text-5xl text-primary">
+          <div className="flex items-center justify-evenly w-full mt-5 text-5xl">
             <BiWifi />
             <BiTv />
             <BiHotel />
             <BiShower />
           </div>
         </div>
+        <div className="absolute right-0 w-[800px] h-[600px] bg-primary/50 -z-10"></div>
       </div>
     </div>
   );
--- app/habitaciones/components/RoomServices.tsx
+++ app/habitaciones/components/RoomServices.tsx
@@ -1,5 +1,67 @@
+import Image from "next/image";
+
+const roomServices = [
+  {
+    img: "/breakfast-1.png",
+    title: "Con desayuno",
+    description:
+      "Incluí el desayuno en tu reserva y comenzá cada mañana con una selección de productos frescos: panes caseros, facturas, frutas, cereales, infusiones y jugos.",
+  },
+  {
+    img: "/breakfast-2.png",
+    title: "Con media pensión",
+    description:
+      "Elegí media pensión y disfrutá, además del desayuno, de una cena completa en nuestro restaurante.",
+  },
+  {
+    img: "/breakfast-3.png",
+    title: "Con pensión completa",
+    description:
+      "Con la pensión completa, viví una experiencia sin preocupaciones: desayuno, almuerzo y cena incluidos.",
+  },
+];
+
 const RoomServices = () => {
-  return <div className="w-full bg-white min-h-[600px]">RoomServices</div>;
+  return (
+    <div className="w-full bg-white min-h-[700px] flex items-center">
+      <div className="container flex items-start justify-between gap-20 mx-auto">
+        {/* Intro */}
+        <div className="flex flex-col gap-8 items-start justify-center max-w-xs">
+          <h2 className="text-5xl font-bold text-primary font-title text-shadow-sm text-shadow-dark/30">
+            Sabor Local
+          </h2>
+          <p className="text-dark">
+            En nuestro hotel, cada comida es parte de la experiencia. Desde un
+            desayuno fresco y variado para comenzar el día con energía, hasta
+            opciones de media pensión y pensión completa que combinan lo mejor
+            de la cocina regional.
+          </p>
+        </div>
+        {/* Galeria */}
+        <div className="grid grid-cols-1 gap-10 place-content-center md:grid-cols-2 lg:grid-cols-3 w-full">
+          {roomServices.map((service, index) => (
+            <div
+              key={index}
+              className="flex flex-col items-center overflow-hidden h-[450px] rounded-lg group shadow-lg shadow-black/30"
+            >
+              <div className="min-h-[250px] w-full relative">
+                <Image
+                  alt="img"
+                  src={service.img}
+                  fill
+                  className="object-cover"
+                />
+              </div>
+              <div className="flex flex-col items-start justify-start h-full gap-3 p-5 bg-background text-dark">
+                <p className="text-xl font-bold">{service.title}</p>
+                <p className="text-dark/80">{service.description}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default RoomServices;
