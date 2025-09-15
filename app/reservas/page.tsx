dbff2f7c33a2f06d739953c2be85b9602adcf723
Abel Acuña <ale_lasarte@hotmail.com>
2025-09-15 08:37:45 -0300

    Change Reservation Size

--- app/reservas/page.tsx
+++ app/reservas/page.tsx
@@ -5,23 +5,23 @@ export default function Reservations() {
   return (
     <main>
       <PagesHero bgUrl="/outside.webp" />
-      <div className="container flex flex-col items-center justify-center gap-10 mx-auto md:my-20 py-20 px-4 sm:px-6 lg:px-8 max-w-7xl">
-        <h2 className="text-4xl sm:text-5xl font-bold text-primary font-title text-shadow-sm text-shadow-dark/30 text-center">
+      <div className="container flex flex-col items-center justify-center gap-10 px-4 py-20 mx-auto md:my-20 sm:px-6 lg:px-8 max-w-7xl">
+        <h2 className="text-4xl font-bold text-center sm:text-5xl text-primary font-title text-shadow-sm text-shadow-dark/30">
           Reservá tu descanso ideal
         </h2>
-        <p className="text-center text-gray-700 max-w-2xl mb-10 text-base sm:text-lg px-2">
+        <p className="max-w-2xl px-2 mb-10 text-base text-center text-gray-700 sm:text-lg">
           Elegí la habitación perfecta para vos y asegurá tu lugar en uno de los
           destinos termales más reconocidos de Argentina. Reservá en pocos
           pasos, sin complicaciones.
         </p>
         <div
-          className="w-full rounded-xl overflow-hidden shadow-lg md:mt-10"
+          className="w-full overflow-hidden shadow-lg rounded-xl md:mt-10"
           style={{ maxWidth: "1200px", height: "auto" }}
         >
           <iframe
             src="https://wubook.net/nneb/bk?f=today&n=1&ep=30d47ca5&o=1.0.0.0"
             title="Motor de reservas Ambassador"
-            className="w-full h-[600px] sm:h-[700px] md:h-[800px] border-0"
+            className="w-full h-screen border-0"
             loading="lazy"
           />
         </div>
